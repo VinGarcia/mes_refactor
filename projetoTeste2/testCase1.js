7b45c1f8651066e96430affe7b70c1b3ae634d8d
Vinícius Garcia <vingarcia00@gmail.com>
2014-11-27 17:13:08 -0200

    - Add two testCase project folders with javascript files.

---
+++ projetoTeste2/testCase1.js
@@ -0,0 +1,43 @@
+
+function myClass() {
+  var privAttr1 = 5;
+  var privAttr2=6;
+  var privFunc1 = function(){console.log('privFun!');}
+  var privFunc2=function(){console.log('privFun!');}
+  this.attr1 = 10;
+  this.attr2 = 20;
+  this.fun1 = function(value, value2) {
+    console.log("fun1, val: " + value);
+  }
+  this.fun2 = function() {
+    console.log("fun2, val: " + value);
+    this.attr1 = 20;
+    this.attr2 = 30;
+  }
+}
+
+myFunc = function() {
+  console.log("myFunc");
+}
+
+// myFunc();
+
+c = new myClass();
+
+// c.fun1(c.attr1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
